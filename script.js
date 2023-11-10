64ee4dd7de2f3f614ed4e60ee5526c3948ab7ead
ViNu-23 <vijayvinu46@gmail.com>
2023-11-10 22:55:47 +0530

    finale

--- script.js
+++ script.js
@@ -8,9 +8,36 @@ function calculate(e) {
   const monthly_payment = document.getElementById("monthly_payment");
   const total_amount = document.getElementById("total_amount");
   const total_interest = document.getElementById("total_interest");
+
   e.preventDefault();
 
   const principal = parseFloat(amount.value);
-  
-  console.log(principal);
+  const calculatedInterest = parseFloat(interest.value)/100/12;
+  const calculatedPayment = parseFloat(years.value)*12
+  const x = Math.pow(1 + calculatedInterest,calculatedPayment);
+  const monthly = (principal*x*calculatedInterest)/(x-1);
+
+if(isFinite(monthly)){
+    monthly_payment.value = monthly.toFixed(2);
+    total_amount.value = (monthly * calculatedPayment).toFixed(2);
+    total_interest.value = (monthly*calculatedPayment-principal).toFixed(2);
+    document.getElementById('results').style.display='block'
 }
+else{
+    showAlert('Please Enter Amount');
+}
+
+}
+function showAlert(error) {
+  const errorDiv = document.createElement('div');
+  errorDiv.className='alert alert-danger';
+  errorDiv.appendChild(document.createTextNode(error));
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading')
+  card.insertBefore(errorDiv,heading); 
+
+}
+setTimeout(function(){
+const msg = document.getElementsByClassName('.alert');
+msg.remove();
+},3000)
